222ea0eecdd3b671d15c7582a06ed0835fe57d1b
Noy <noe.dangla@epita.fr>
2024-03-12 15:55:57 +0100

    automatic commit

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ fetch('https://4noy.github.io/static/header.html')
         })
         .catch(error => console.error('Error fetching HTML:', error));
 
+
+function hash(string) {
+  const utf8 = new TextEncoder().encode(string);
+  return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+      .map((bytes) => bytes.toString(16).padStart(2, '0'))
+      .join('');
+    return hashHex;
+  });
+}
+
 function checkFlag(inputId, resultId, correctHash) {
   var input = document.getElementById(inputId).value;
   var resultElement = document.getElementById(resultId);
   
   // Hasher l'entrée utilisateur avec SHA-256
-  var hashedInput = sha256(input);
+  var hashedInput = hash(input);
   
   // Vérifier si le hash de l'entrée correspond au hash correct
   if (hashedInput === correctHash) {
