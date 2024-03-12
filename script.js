450c0ea62790df937651a75f9cf8b726afe44301
Noy <noe.dangla@epita.fr>
2024-03-12 18:56:37 +0100

    automatic commit

--- script.js
+++ script.js
@@ -5,6 +5,30 @@ fetch('https://4noy.github.io/static/header.html')
         })
         .catch(error => console.error('Error fetching HTML:', error));
 
+let hashes = [
+  "26010c5a26090f38d288cd8a00c38b41548a3e70f77ce3ecd487d819986b2b28",
+  "c11a610981a5251aea302e862267c2b389b5ecff233708881f5beabdf614aa1b",
+  "51b1463f66df7462b15056f4dc9d6a5d9a43e676b5e793e36578580487b69377",
+  "2b84da5ec8f1973fef408f285613191ba5c9898185febc0560f004960471f0a2",
+  "1105ca72602e35e8c9b011366fc8dcb04450a952892bf971b65d7316ff056623",
+  "90d5a0ce78165291e1b4ed4da92dc58d83256cb874798cb3c47c5c683309fa47",
+  "votre_hash_correcte_7",
+  "votre_hash_correcte_8",
+  "votre_hash_correcte_9"
+];
+
+for (let i = 1; i <= 9; i++) {
+  let flagId = "flag" + i;
+  let resultId = "result" + i;
+  let hash = hashes[i - 1];
+
+  document.getElementById(flagId).addEventListener("keyup", function(event) {
+    handleEnterKeyPress(event, flagId, resultId, hash);
+  });
+}
+
+
+
 
 function hash(string) {
   const utf8 = new TextEncoder().encode(string);
